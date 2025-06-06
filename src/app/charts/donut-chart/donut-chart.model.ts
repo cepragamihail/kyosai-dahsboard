035e0ed6bdb2d6f02eb9087087a6eef14ff6c616
Mihail Cepraga <yahim09@gmail.com>
2025-06-06 15:08:20 +0300

    init Donut chart

---
+++ src/app/charts/donut-chart/donut-chart.model.ts
@@ -0,0 +1,5 @@
+export interface DonutChartModel {
+  name: string,
+  value: number,
+  color: number
+}
---
+++ src/app/charts/donut-chart/donut-chart.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-chart',
+  imports: [],
+  template: `
+    <p>
+      donut-chart works!
+    </p>
+  `,
+  styles: ``
+})
+export class DonutChart {
+
+}
