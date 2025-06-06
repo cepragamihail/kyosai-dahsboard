34a3fb7162b8ec63e3ca08034aaf7f063c5019b2
Mihail Cepraga <yahim09@gmail.com>
2025-06-06 15:11:35 +0300

    add factory report as service return data type

--- src/app/services/factory.service.ts
+++ src/app/services/factory.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment'
+import {FactoryReportModel} from './factory-report.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,10 @@ export class FactoryService {
 
   private http = inject(HttpClient);
 
-  getFactoryReports(): Observable<any> {
+  getFactoryReports(): Observable<FactoryReportModel> {
     const url = `${environment.api_path}${environment.factory_reports}`;
     console.log(url);
-    return this.http.get<any>(url);
+    return this.http.get<FactoryReportModel>(url);
   }
 
 }
