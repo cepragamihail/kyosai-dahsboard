b6880fc156b4c8d4979345f9787e351192089154
yahim09 <yahim09@gmail.com>
2025-06-06 10:41:11 +0000

    ad example of guage chart

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -4,9 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { FactoryService } from '../services/factory.service';
 import { Observable } from 'rxjs';
+import { GuageChart } from '../charts/guage-chart/guage-chart';
+import { GaugeModel } from '../charts/guage-chart/guage.model';
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, GuageChart],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
@@ -24,5 +26,44 @@ export class Dashboard {
     //   console.log(data);
     // });
   }
+
+  // id?: number,
+  // title: string,
+  // shortTitle: string,
+  // value: number,
+  // sectors: number[],
+  // legend: GaugeLegendModel[],
+
+get oee(): GaugeModel {
+   return {
+    "title": "oee",
+    "shortTitle": "oee",
+    "value": 80,
+    "sectors": [30, 55, 65, 100],
+    "legend": [
+      {
+        "value": 14,
+        "sectors": [30, 55, 65, 100],
+        "label": "Availability",
+        "short_label": "A",
+        "units": "%"
+      },
+      {
+        "value": 61,
+        "sectors": [60, 75, 85, 100],
+        "label": "Performance",
+        "short_label": "P",
+        "units": "%"
+      },
+      {
+        "value": 90,
+        "sectors": [85, 90, 95, 100],
+        "label": "Quality",
+        "short_label": "Q",
+        "units": "%"
+      }
+    ]
+  };
+}
 
 }
