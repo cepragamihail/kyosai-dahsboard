ce43718884bc0f5a2b08c76731d3c7cb08acb8f2
Mihail Cepraga <yahim09@gmail.com>
2025-06-06 15:08:00 +0300

    init Bar Chart

---
+++ src/app/charts/bar-chart/bar-chart.model.ts
@@ -0,0 +1,7 @@
+export interface BarChartModel {
+  name: string;
+  value: string;
+  valueAlias: string;
+  color?: string;
+  unit?: string;
+}
---
+++ src/app/charts/bar-chart/bar-chart.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-chart',
+  imports: [],
+  template: `
+    <p>
+      bar-chart works!
+    </p>
+  `,
+  styles: ``
+})
+export class BarChart {
+
+}
